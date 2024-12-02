0da16ad5568df10a95ac8c375f186f22b454fc88
Suraj Kumar <suraj.22scse1310015@galgotiasuniversity.edu.in>
2024-12-02 12:15:54 +0530

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Batch -1 on 25/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
